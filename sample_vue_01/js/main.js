166c79213e27611f023eec2d825a1df95e58dea8
大塚　泰司 <yasushi-ohtsuka@nifty.ne.jp>
2021-12-18 03:04:50 +0900

    vue.jsの練習

---
+++ sample_vue_01/js/main.js
@@ -0,0 +1,15 @@
+(function() {
+  'use strict';
+
+  var vm = new Vue({
+    el: '#app',
+    data: {
+      name: 'yasushi-jp',
+      todos:[
+        'task 1',
+        'task 2',
+        'task 3',
+      ]
+    }
+  });
+})();
